eec8064fd7eec4c58735c69b85d6e196e6fef17c
顾杰 <gujie@daluyundun.com.cn>
2021-04-08 10:45:29 +0800

    方法引用

--- java8-study/src/main/java/cn/jackiegu/java8/study/method/references/MethodReferencesTest.java
+++ java8-study/src/main/java/cn/jackiegu/java8/study/method/references/MethodReferencesTest.java
@@ -2,8 +2,10 @@ package cn.jackiegu.java8.study.method.references;
 
 import cn.jackiegu.technology.common.util.LoggerUtil;
 
-import java.util.Collections;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -15,30 +17,44 @@ import java.util.function.Supplier;
 public class MethodReferencesTest {
 
     public static void main(String[] args) {
-        // 构造器引用
-        LoggerUtil.info("构造器引用");
-        // Cat cat1 = Cat.create(() -> new Cat());
-        Cat cat1 = Cat.create(Cat::new);
-        cat1.setName("球球1");
-        cat1.setAge(4);
-        cat1.setSex("男");
-        System.out.println(cat1);
-
-        // 静态方法引用
-        LoggerUtil.info("静态方法引用");
-        List<Cat> cats = Collections.singletonList(cat1);
-        // cats.forEach(cat -> Cat.call(cat));
-        cats.forEach(Cat::call);
-
-        // 类的成员方法引用
-        LoggerUtil.info("类的成员方法引用");
-        Cat cat2 = new Cat("球球2", 4, "男");
-        // Supplier<String> supplier = () -> qiuqiu.getName();
-        Supplier<String> supplier = cat2::getName;
-        System.out.println(supplier.get());
-
+        MethodReferencesEntity[] entities = new MethodReferencesEntity[]{
+            new MethodReferencesEntity("张三", LocalDate.of(1993, 8, 5)),
+            new MethodReferencesEntity("李四", LocalDate.of(1992, 7, 1)),
+            new MethodReferencesEntity("王五", LocalDate.of(1995, 10, 28)),
+            new MethodReferencesEntity("赵六", LocalDate.of(1994, 3, 15)),
+            new MethodReferencesEntity("钱七", LocalDate.of(1992, 7, 1))
+        };
         // 对象引用::实例方法名
         LoggerUtil.info("对象引用::实例方法名");
-        cats.forEach(cat1::repair);
+        MethodReferencesEntity[] e1 = entities.clone();
+        MethodReferencesEntityCompare compare = new MethodReferencesEntityCompare();
+        // Arrays.sort(e1, (a, b) -> compare.compareByBirthday(a, b));
+        Arrays.sort(e1, compare::compareByBirthday);
+        System.out.println(Arrays.toString(e1));
+
+        // 类名::静态方法名
+        LoggerUtil.info("类名::静态方法名");
+        MethodReferencesEntity[] e2 = entities.clone();
+        // Arrays.sort(e2, (a, b) -> a.getBirthday().compareTo(b.getBirthday()));
+        Arrays.sort(e2, MethodReferencesEntity::compareTo);
+        System.out.println(Arrays.toString(e2));
+
+        // 类名::实例方法名
+        LoggerUtil.info("类名::实例方法名");
+        // BiPredicate<MethodReferencesEntity, MethodReferencesEntity> bp = (a, b) -> a.isBirthdaySame(b);
+        BiPredicate<MethodReferencesEntity, MethodReferencesEntity> bp = MethodReferencesEntity::isBirthdaySame;
+        System.out.println(bp.test(entities[1], entities[4]));
+
+        // 类名::new
+        LoggerUtil.info("类名::new");
+        // Supplier<MethodReferencesEntity> supplier = () -> new MethodReferencesEntity();
+        Supplier<MethodReferencesEntity> supplier = MethodReferencesEntity::new;
+        System.out.println(supplier.get());
+
+        // 类型[]::new
+        LoggerUtil.info("类型[]::new");
+        // Function<Integer, MethodReferencesEntity[]> function = i -> new MethodReferencesEntity[i];
+        Function<Integer, MethodReferencesEntity[]> function = MethodReferencesEntity[]::new;
+        System.out.println(Arrays.toString(function.apply(3)));
     }
 }
