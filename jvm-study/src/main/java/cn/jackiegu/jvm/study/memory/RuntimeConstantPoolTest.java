41563027e6725f6cb8754a40afb9bc587d3eea75
顾杰 <gujie@daluyundun.com.cn>
2021-05-08 16:15:29 +0800

    新增测试方法

--- jvm-study/src/main/java/cn/jackiegu/jvm/study/memory/RuntimeConstantPoolTest.java
+++ jvm-study/src/main/java/cn/jackiegu/jvm/study/memory/RuntimeConstantPoolTest.java
@@ -20,5 +20,19 @@ public class RuntimeConstantPoolTest {
     public static void main(String[] args) {
         String s = "hello";
         System.out.println("s = " + s);
+
+        /*
+         * "计算机软件"一词由于是第一次出现, 因此intern()返回的引用和由StringBuilder创建的字符串实例就是同一个
+         *  所以结果为true
+         */
+        String s1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(s1.intern() == s1);
+
+        /*
+         * "java"一词由于不是第一次出现, 早在sun.misc.Version这个类的时候就已进入了常量池中, 因此intern()返回的引用和由StringBuilder创建的字符串实例不是同一个
+         *  所以结果为false
+         */
+        String s2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(s2.intern() == s2);
     }
 }
